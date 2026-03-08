03e23b34b87d8a8d80370c1a98d9ca8befcadfe0
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-03-07 22:59:33 -0500

    feat: wire up WebSocket signaling and serve script.js for WebRTC peer connection

--- main.go
+++ main.go
@@ -1,11 +1,49 @@
 package main
 
-import "fmt"
-import "net/http"
-import "html/template"
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
 
 var username string
 var password string
+
+var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+var clients = make(map[*websocket.Conn]bool)
+var mu sync.Mutex
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	mu.Lock()
+	clients[conn] = true
+	mu.Unlock()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			mu.Lock()
+			delete(clients, conn)
+			mu.Unlock()
+			break
+		}
+		mu.Lock()
+		for c := range clients {
+			if c != conn {
+				c.WriteMessage(websocket.TextMessage, msg)
+			}
+		}
+		mu.Unlock()
+	}
+}
 func login(w http.ResponseWriter, r *http.Request){
 	if r.Method == http.MethodPost{
 		r.ParseForm()
@@ -34,6 +72,10 @@ func main(){
 	mux := http.NewServeMux()
 	mux.Handle("/login",http.HandlerFunc(login))
 	mux.Handle("/",http.HandlerFunc(homepage))
+	mux.Handle("/ws",http.HandlerFunc(wsHandler))
+	mux.Handle("/script.js",http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "script.js")
+	}))
 
 	server := http.Server{
 			Addr: ":8080",
